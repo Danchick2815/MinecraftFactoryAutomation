22d617d91986099461ef8dcd5d70c28df243b907
Danchick2815 <dmawa_ll@mail.ru>
2026-01-18 20:17:10 +0300

    Added template converter

---
+++ resources/converters/template/converter.lua
@@ -0,0 +1,12 @@
+local function name()
+    return ""
+end
+
+local function send()
+    return '{}'
+end
+
+local function command(data)
+end
+
+return {name = name, send = send, command = command}
